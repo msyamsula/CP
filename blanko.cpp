1f4089ee2b34cab48bd8cc8b223be79790576197
Syamsul <msyamsula1995@gmail.com>
2020-11-12 15:23:58 +0700

    update

---
+++ blanko.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vl> vvl;
+typedef vector<vs> vvs;
+typedef vector<vi> vvi;
+
+
+
+int main(){
+    cout << "wikwik" << endl;
+    return 0;
+}
